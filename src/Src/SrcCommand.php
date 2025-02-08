88682b1d8d1353c6208c9b6a88ce6371f990a22c
david_smith <david_smith@sweetwater.com>
2025-02-08 16:12:34 -0500

    feat/orders Updated `src/Src/SrcCommand.php`.

--- src/Src/SrcCommand.php
+++ src/Src/SrcCommand.php
@@ -1,12 +1,10 @@
 <?php
 
-namespace Zerotoprod\:namespace\Src;
+namespace Zerotoprod\SpapiOrdersCli\Src;
 
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 #[AsCommand(
@@ -15,18 +13,12 @@ use Symfony\Component\Console\Output\OutputInterface;
 )]
 class SrcCommand extends Command
 {
-    public const signature = ':slug:src';
+    public const signature = 'spapi-orders-cli:src';
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $output->writeln('https://github.com/zero-to-prod/:slug');
+        $output->writeln('https://github.com/zero-to-prod/spapi-orders-cli');
 
         return Command::SUCCESS;
-    }
-
-    public function configure(): void
-    {
-        $this->addArgument('argument', InputArgument::REQUIRED);
-        $this->addOption('', mode: InputOption::VALUE_OPTIONAL, description: '', suggestedValues: ['']);
     }
 }
